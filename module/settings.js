67b39f7dc9d4b6a912dbe98243822524b40c6b2d
mfventura <manuel.fdz.ventura@gmail.com>
2023-06-13 18:36:43 +0100

    Agregado fichero de settings. Agregada opcion para activar y desactivar magia (WIP)

--- module/settings.js
+++ module/settings.js
@@ -10,7 +10,7 @@ export default function registerSystemSettings() {
     });
 
     // Enable magic system
-    game.settings.register("ryf", "magicEnabled", {
+    game.settings.register("ryf", "advanced.magicEnabled", {
         name: "Activar Magia",
         hint: "Incluye los items de tipo Hechizo y activa la pestaña de hechizos en la ficha de personaje.",
         scope: "world",
