acad7238b59132d90ba6e553b10ae6be85967049
mfventura <manuel.fdz.ventura@gmail.com>
2023-06-29 16:05:31 +0100

    Tirada daño desde el chat al acertar ataque

--- module/sheets/RyFExtras.js
+++ module/sheets/RyFExtras.js
@@ -1,3 +1,5 @@
+import {tiradaDano} from "../tiradas/tirada-dano.js";
+
 export default class RyFExtras {
   static chatListeners (html) {
     html.on('click', '.danoDesdeChat', this._onDanoDesdeChat.bind(this));
@@ -35,51 +37,24 @@ export default class RyFExtras {
     );
   }
 
-   static _onDanoDesdeChat (event){
+  static _onDanoDesdeChat (event){
      const element = event.currentTarget;
      const dataset = element.dataset;
-     if (!dataset.exito){
-      return 1;
-    }
-     let tirada=dataset.dano +"+"+dataset.bonos;
-     if(dataset.efecto && dataset.efecto > 0){
-      tirada = tirada + "+" + dataset.efecto+"d6x"
-     }
-     let objetivo_id=""
-     let d6Roll = new Roll(tirada).roll({async: false});
-     let total= 0;
-     let resultado="";
-     let absorcion_armadura=0;
-     if (dataset.absorcion_armadura){absorcion_armadura=dataset.absorcion_armadura}
-     resultado=eval(d6Roll.result);
-     total=Number(resultado)-Number(absorcion_armadura);
      let objetivo;
+     let numeroDados = Number(dataset.danonumerodados);
+     let bonificador = dataset.danobonificador;
+     let absorcion=0;
+     let nombreArma = dataset.arma;
+     console.log(dataset)
+     if(dataset.efecto && dataset.efecto > 0){
+      numeroDados += Number(dataset.efecto);
+     }
+     
+     if (dataset.absorcion_armadura){absorcion=dataset.absorcion_armadura}
      if (dataset.objetivo){
        objetivo=canvas.tokens.get(dataset.objetivo);
-       objetivo_id=objetivo;
      }
-     let causa_dano=total > 0 && objetivo;
-     const archivo_template_chat = '/systems/ryf/templates/dialogs/tiradaDanoChat.html';
-     const datos_template_chat = {
-                             tirada: tirada,
-                             nom_habilidad: dataset.arma,
-                             armadura: absorcion_armadura,
-                             resultado: resultado,
-                             total: total,
-                             objetivo: objetivo_id,
-                             causa_dano: causa_dano
-                           };
-    var contenido_Dialogo_chat;
-    renderTemplate(archivo_template_chat, datos_template_chat).then(
-    (contenido_Dialogo_chat)=> {
-      const chatData = {
-             type: CONST.CHAT_MESSAGE_TYPES.ROLL,
-             roll: d6Roll,
-             content: contenido_Dialogo_chat,
-         };
-      ChatMessage.create(chatData);
-    } )
-
+     tiradaDano(nombreArma, objetivo, absorcion, bonificador, numeroDados);
   }
 
 
